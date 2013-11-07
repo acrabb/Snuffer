c8a3bae1e397de0e41c5bf1a404691a466b83473
André Crabb <crabb.andre@gmail.com>
2013-11-06 18:17:35 -0800

    created project & repo

---
+++ src/crabb/andre/Snuffer/Snuffer.java
@@ -0,0 +1,15 @@
+package crabb.andre.Snuffer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Snuffer extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
